ebee430561c925de4cf853f05e243ba6ecb6dbb7
pri0111 <priyamanna0111@gmail.com>
2025-04-29 13:25:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const githubLinks = {
+    "1990": "https://pri0111.github.io/HYDRABAD_1990/",
+    "2000": "https://pri0111.github.io/HYDRABAD_2000/",
+    "2010": "https://pri0111.github.io/HYDRABAD_2010/",
+    "2020": "https://pri0111.github.io/HYDRABAD_2020/"
+  };
